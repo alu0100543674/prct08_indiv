dba5417d5dc56976b2ee4b468617935539e523be
alu0100543674 <Carla Hernandez>
2013-11-05 20:29:49 +0000

    De nuevo

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -8,19 +8,6 @@ class Matriz
       @columnas = matriz[0].size
    end
 
-   def +(other)
-    i = 0
-    j = 0
-    suma = @matriz
-    while i < @filas      
-      while j < @columnas
-        suma[i][j] = suma[i][j] + other.matriz[i][j]
-        j += 1
-      end
-      i += 1
-      j = 0
-    end
-    Matriz.new(suma)
-   end
+
 
 end
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -2,44 +2,29 @@ require 'matriz.rb'
 
 describe Matriz do
   before :each do
-     @matriz1 = Matriz.new([[1, 1], [1, 1]])
-     @matriz2 = Matriz.new([[1, 1], [1, 1]])
+     #@matriz1 = Matriz.new([[1, 1], [1, 1]])
+     #@matriz2 = Matriz.new([[1, 1], [1, 1]])
+     #@msum = Matriz.new ([[2, 2], [2, 2]])
 
-     #@matriz1 = Matriz.new(2,2)
-     #@matriz1[0] = [1, 1]
-     #@matriz1[1] = [1, 1]
+     @matriz1 = Matriz.new(2,2)
+     @matriz1[0] = [2, 2]
+     @matriz1[1] = [2, 2]
 
-     #@matriz2 = Matriz.new(2,2)
-     #@matriz2[0] = [1, 1]
-     #@matriz2[1] = [1, 1]
-      
+     @matriz2 = Matriz.new(2,2)
+     @matriz2[0] = [1, 1]
+     @matriz2[1] = [1, 1]
+     
+     @matrizsuma = Matriz.new(2,2)
+     @matrizsuma[0] = [3, 3]
+     @matrizsuma[1] = [3, 3]
+ 
+     @matrizresta = Matriz.new(2,2)
+     @matrizresta[0] = [1, 1]
+     @matrizresta[1] = [1, 1]
+
+
   end 
    
-  describe " Almacenamiento de matrices " do
-     it "Deber tener columnas matriz1 " do
-        #(defined?(@matriz1.columnas)).should be_true
-        @matriz1.columnas.should eq(2)
-     end
-     it " Debe tener filas matriz1 " do
-        #(defined?(@matriz1.filas)).should be_true
-        @matriz1.filas.should eq(2)
-     end
+ 
 
-      it "Deber tener columnas matriz2 " do
-        #(defined?(@matriz2.columnas)).should be_true
-        @matriz2.columnas.should eq(2)
-     end
-     it " Debe tener filas matriz2 " do
-        #(defined?(@matriz2.filas)).should be_true
-        @matriz2.filas.should eq(2)
-     end
   end
-
-  describe " Suma " do
-     it " Suma de matrices " do
-       #sum = @matriz1 + @matriz2
-       @matriz5 = Matriz.new([[1, 1], [1, 1]])
-       (@matriz1 + @matriz5).matriz.should  == "[[2, 2], [2, 2]]"
-     end
-  end
-end
