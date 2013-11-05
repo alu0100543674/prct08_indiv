5227f26d27b9389be1bad8c77302b43182b4b1af
alu0100543674 <Carla Hernandez>
2013-11-05 14:29:59 +0000

    Creamos matriz1 y matriz2 en matriz_spec.rb

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,1 +1,14 @@
 require 'matriz.rb'
+
+describe Matriz do
+  before :each do
+     @matriz1 = Matriz.new(2,2)
+     @matriz1[0] = [1, 1]
+     @matriz1[1] = [1, 1]
+
+     @matriz2 = Matriz.new(2,2)
+     @matriz2[0] = [1, 1]
+     @matriz2[1] = [1, 1]
+      
+  end 
+end
