fbea9e3d9410a0044b8d2fe99582a234b95f3a62
alu0100543674 <Carla Hernandez>
2013-11-05 15:18:00 +0000

    Metodo filas y columnas definidos

--- lib/matriz.rb~
+++ lib/matriz.rb~
@@ -1,2 +1,10 @@
 class Matriz
+   attr_reader :filas, :columnas, :matriz
+ 
+   def initialize(matriz)
+      @matriz = matriz
+      @filas = matriz.size;
+      @columnas = matriz[0].size
+   end
+
 end
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -2,13 +2,38 @@ require 'matriz.rb'
 
 describe Matriz do
   before :each do
-     @matriz1 = Matriz.new(2,2)
-     @matriz1[0] = [1, 1]
-     @matriz1[1] = [1, 1]
+     @matriz1 = Matriz.new([[1, 1], [1, 1]])
+     @matriz2 = Matriz.new([[1, 1], [1, 1]])
 
-     @matriz2 = Matriz.new(2,2)
-     @matriz2[0] = [1, 1]
-     @matriz2[1] = [1, 1]
+     #@matriz1 = Matriz.new(2,2)
+     #@matriz1[0] = [1, 1]
+     #@matriz1[1] = [1, 1]
+
+     #@matriz2 = Matriz.new(2,2)
+     #@matriz2[0] = [1, 1]
+     #@matriz2[1] = [1, 1]
       
   end 
+   
+  describe " Almacenamiento de MATRIZ1 " do
+     it "Deber tener columnas " do
+        #(defined?(@matriz1.columnas)).should be_true
+        @matriz1.columnas.should eq(2)
+     end
+     it " Debe tener filas " do
+        #(defined?(@matriz1.filas)).should be_true
+        @matriz1.filas.should eq(2)
+     end
+
+      it "Deber tener columnas " do
+        #(defined?(@matriz2.columnas)).should be_true
+        @matriz2.columnas.should eq(2)
+     end
+     it " Debe tener filas " do
+        #(defined?(@matriz2.filas)).should be_true
+        @matriz2.filas.should eq(2)
+     end
+
+
+  end
 end
