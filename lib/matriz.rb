c69b1c98b405b113fa591eb4c0c7d59b5895c11b
alu0100543674 <Carla Hernandez>
2013-11-06 23:10:14 +0000

    EMPEZADO DE NUEVO

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -1,13 +1,4 @@
 require 'matrix'
 class Matriz
-   attr_reader :filas, :columnas, :matriz
- 
-   def initialize(matriz)
-      @matriz = matriz
-      @filas = matriz.size;
-      @columnas = matriz[0].size
-   end
-
-
 
 end
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,30 +1,5 @@
 require 'matriz.rb'
 
 describe Matriz do
-  before :each do
-     #@matriz1 = Matriz.new([[1, 1], [1, 1]])
-     #@matriz2 = Matriz.new([[1, 1], [1, 1]])
-     #@msum = Matriz.new ([[2, 2], [2, 2]])
 
-     @matriz1 = Matriz.new(2,2)
-     @matriz1[0] = [2, 2]
-     @matriz1[1] = [2, 2]
-
-     @matriz2 = Matriz.new(2,2)
-     @matriz2[0] = [1, 1]
-     @matriz2[1] = [1, 1]
-     
-     @matrizsuma = Matriz.new(2,2)
-     @matrizsuma[0] = [3, 3]
-     @matrizsuma[1] = [3, 3]
- 
-     @matrizresta = Matriz.new(2,2)
-     @matrizresta[0] = [1, 1]
-     @matrizresta[1] = [1, 1]
-
-
-  end 
-   
- 
-
-  end
+end
